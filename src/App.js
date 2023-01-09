60151f22dafac94b58b960f706005b06daf6eeae
Muhammed Fawaz MV <mvfawazmfz@gmail.com>
2023-01-10 04:43:36 +0530

    Fronted work finished

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router,Route } from 'react-router-dom';
+import './App.css';
+
+/**
+ * ?  =====Import Components=====
+ */
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
